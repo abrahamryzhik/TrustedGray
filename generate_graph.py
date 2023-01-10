a49b77bb2389c39219d9886810b0b663946e4d20
abrahamryzhik <aryzhik@gmail.com>
2023-01-10 01:52:12 -0800

    code to determine average and min price, generate graph

---
+++ generate_graph.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+lines = []
+
+with open("price_history.txt", "r") as f:
+	lines = f.readlines()
+
+
+date_time = []
+
+prices = []
+
+for entry in lines:
+	entry_split = entry.split(',')
+	date_time.append(entry_split[0])
+	prices.append(float(entry_split[1]))
+
+
+#date_time = ["2021-01-01", "2021-01-02", "2021-01-03"]
+date_time = pd.to_datetime(date_time)
+#data = [1, 2, 3]
+
+DF = pd.DataFrame()
+DF['value'] = prices
+DF = DF.set_index(date_time)
+plt.plot(DF)
+plt.gcf().autofmt_xdate()
+plt.title("16570 Price History")
+plt.ylabel("Average Price ($)")
+plt.savefig("16570_price_graph.png")
--- get_prices.py
+++ get_prices.py
@@ -19,23 +19,8 @@ from bs4 import BeautifulSoup
 from datetime import date
 
 
-# options = Options()
-# ua = UserAgent()
-# userAgent = ua.random
-# #print(userAgent)
-# options.add_argument(f'user-agent={userAgent}')
-
 url = "https://www.watchfinder.com/find/Rolex/Explorer%20II/16570/1780"
 
-# browser = webdriver.Chrome(os.path.join(os.getcwd(), 'chromedriver'))
-
-
-
-# browser.get(url)
-
-# time.sleep(2)
-
-# innerHTML = browser.execute_script("return document.body.innerHTML")
 
 innerHTML = requests.get(url).text
 
@@ -56,14 +41,14 @@ prices_list = []
 for product_price_tag in product_prices:
     prices_list.append(product_price_tag.string)
 
-print(prices_list)
+#print(prices_list)
 
 with open("prices.txt", "w") as f:
     f.write('\n'.join(prices_list))
 
 products = soup.find_all("div", "prods_item prods_item-stock")
 
-price_linK_list = []
+price_link_list = []
 
 with open("individual_product_html.txt", "w") as f:
     f.write(str(products[0].prettify()))
@@ -72,24 +57,19 @@ for product in products:
     price = product.find("span", "prods_price").string
     link = product.find("a").get("href")
 
-    price_linK_list.append((price, "https://www.watchfinder.com" + str(link)))
-    print(price)
-    print("https://www.watchfinder.com" + str(link))
+    price_link_list.append((price, "https://www.watchfinder.com" + str(link)))
+    # print(price)
+    # print("https://www.watchfinder.com" + str(link))
 
 with open("prices_with_link.txt", "w") as f:
-    f.write('\n'.join([stock_example[0] + " " + stock_example[1] for stock_example in price_linK_list]))
+    f.write('\n'.join([stock_example[0] + " " + stock_example[1] for stock_example in price_link_list]))
 
 today_avg = 0
-p_list = [price_linK_list[i][0].replace(",", "").replace("$","") for i in range(len(price_linK_list))]
-print(p_list)
+p_list = [price_link_list[i][0].replace(",", "").replace("$","") for i in range(len(price_link_list))]
+#print(p_list)
 sum_prices = sum([int(p) for p in p_list])
-#print(x1)
-num_prices = len(price_linK_list)
-#print(x2)
-# print(sum_prices/num_prices)
-# today_avg = sum_prices/num_prices
-# print(today_avg)
-# print(x1/x2)
+
+num_prices = len(price_link_list)
 
 try:
 
@@ -98,9 +78,23 @@ try:
 except:
     today_avg = 0
 
-print(date.today())
-print(today_avg)
+# print(date.today())
+# print(today_avg)
 
+min_price = int(price_link_list[0][0].replace(",", "").replace("$",""))
+
+min_link = price_link_list[0][1]
+
+for i in range(1, len(price_link_list)):
+    cur_price = int(price_link_list[i][0].replace(",", "").replace("$",""))
+
+    if cur_price < min_price:
+        min_price = cur_price
+        min_link = price_link_list[i][1]
+
+print("The avergae price of a 16570 today is $" + str(today_avg))
+print("The cheapest 16570 today is $" + str(min_price))
+print("It is available at " + min_link)
 
 with open("price_history.txt", "a") as f:
     f.write('\n')
