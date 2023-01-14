2f162aa006f66708afc77cf6f5b5fe5de228d134
abrahamryzhik <aryzhik@gmail.com>
2023-01-14 01:32:45 -0800

    isolating link to cheapest item

--- test_script.js
+++ test_script.js
@@ -1,9 +1,3 @@
-var txtFile = new XMLHttpRequest();
-txtFile.open("GET", "https://abrahamryzhik.github.io/TrustedGray/prices.txt", true);
-txtFile.onreadystatechange = function() {
-  if (txtFile.readyState === 4 && txtFile.status == 200) {
-     allText = txtFile.responseText;
-  }
-};
-txtFile.send();
-console.log(allText);
+var txtFile = "this link is avaialable at https://hi.com";
+var linkArray = txtFile.split("avaialable at ");
+console.log(linkArray[1])
